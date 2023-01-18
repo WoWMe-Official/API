f2e962b1975f2467ddc4a516f79f0cfa52f11989
Ferrariic <thearcticfalcon@gmail.com>
2023-01-18 00:46:13 -0600

    configure table values

--- api/database/tables.sql
+++ api/database/tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `account_types` (
  `ID` tinyint NOT NULL,
  `account_type` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `available_days` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `user_id` int NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `available_days` (
  `start_time` datetime NOT NULL,
  `end_time` datetime NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `challenges` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `name` varchar(255) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE `challenges` (
  `organization` int NOT NULL,
  `leaderboard` int NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `challenge_details_day` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `day_hash` tinytext NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE `challenge_details_day` (
  `end_time` datetime NOT NULL,
  `is_start` tinyint(1) NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `events` (
  `id` int NOT NULL AUTO_INCREMENT,
  `hash` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
@@ -43,43 +43,43 @@ CREATE TABLE `events` (
  `difficulty` tinytext NOT NULL,
  PRIMARY KEY (`id`),
  UNIQUE KEY `hash` (`hash`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `fitness_goals` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `user_id` int NOT NULL,
  `goal` tinytext NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `fitness_level` (
  `level` int NOT NULL,
  `description` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `genders` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `gender` tinytext NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `leaderboard` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `name` tinytext NOT NULL,
  `pace` int NOT NULL,
  `distance` int NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `organization` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `name` tinytext NOT NULL,
  `image_route` tinytext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
  `distance` int NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `ratings` (
  `rating_id` bigint NOT NULL AUTO_INCREMENT,
  `rater` int NOT NULL,
  `rated` int NOT NULL,
  `rating` int NOT NULL,
  PRIMARY KEY (`rating_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `registration` (
  `user_id` int NOT NULL AUTO_INCREMENT,
  `email` varchar(320) NOT NULL,
@@ -96,7 +96,7 @@ CREATE TABLE `registration` (
  `timestamp_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  `timestamp_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `relationships` (
  `relationship_id` bigint NOT NULL AUTO_INCREMENT,
  `relationship_type` tinyint NOT NULL,
@@ -105,13 +105,13 @@ CREATE TABLE `relationships` (
  `pending_response` tinyint(1) NOT NULL,
  `target` int NOT NULL,
  PRIMARY KEY (`relationship_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `specializations` (
  `ID` int NOT NULL AUTO_INCREMENT,
  `user_id` int NOT NULL,
  `specialization` tinytext NOT NULL,
  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `stats` (
  `id` int NOT NULL AUTO_INCREMENT,
  `uuid` int NOT NULL,
@@ -119,28 +119,28 @@ CREATE TABLE `stats` (
  `title` varchar(255) NOT NULL,
  `stat` int DEFAULT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `stat_workout_hash` (
  `id` int NOT NULL AUTO_INCREMENT,
  `uuid` int NOT NULL,
  `stat` tinytext NOT NULL,
  `workout` tinytext NOT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `tokens` (
  `token_id` int NOT NULL AUTO_INCREMENT,
  `token` tinytext NOT NULL,
  `user_id` int NOT NULL,
  `auth_level` int NOT NULL,
  PRIMARY KEY (`token_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `trainer_client_history` (
  `session_id` bigint NOT NULL AUTO_INCREMENT,
  `trainer_id` int NOT NULL,
  `client_id` int NOT NULL,
  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `trainer_information` (
  `user_id` int NOT NULL,
  `social_security_number` tinytext NOT NULL,
@@ -148,7 +148,7 @@ CREATE TABLE `trainer_information` (
  `rate` int NOT NULL,
  `payment_method` tinytext NOT NULL,
  `certification_photo` tinytext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `trainer_stats` (
  `trainer_id` int NOT NULL,
  `wallet_balance` decimal(10,0) NOT NULL,
@@ -160,7 +160,7 @@ CREATE TABLE `trainer_stats` (
  `client_count` int NOT NULL,
  `steps` int NOT NULL,
  `distance` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `user_information` (
  `user_id` int NOT NULL,
  `height_ft_in` decimal(10,0) NOT NULL,
@@ -169,7 +169,7 @@ CREATE TABLE `user_information` (
  `weight_kg` int NOT NULL,
  `body_fat_percentage` int NOT NULL,
  `fitness_level` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `workout` (
  `id` int NOT NULL AUTO_INCREMENT,
  `hash` tinytext NOT NULL,
@@ -178,7 +178,7 @@ CREATE TABLE `workout` (
  `reps` int DEFAULT NULL,
  `weight` float DEFAULT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 CREATE TABLE `workout_plan` (
  `id` int NOT NULL AUTO_INCREMENT,
  `uuid` int NOT NULL,
@@ -187,4 +187,4 @@ CREATE TABLE `workout_plan` (
  `workouts_completed` int DEFAULT NULL,
  `fitness_level` varchar(255) DEFAULT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
