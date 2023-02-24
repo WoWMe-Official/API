54adea20bc2161ac13659fda4fa8f83da99ca855
Ferrariic <thearcticfalcon@gmail.com>
2023-02-23 22:42:45 -0600

    Update inbox.py

--- api/routers/inbox.py
+++ api/routers/inbox.py
@@ -13,42 +13,10 @@ from api.database.models import Event, Inbox, Bcc, Cc
 from api.routers.functions.general import get_token_user_id
 from api.routers.models.inbox import inbox, bcc, cc, inbox_conversation_start
 from api.database.functions import generate_token
+import itertools
+import operator
 
 router = APIRouter()
-
-
-# class inbox(BaseModel):
-#     inbox_id: int
-#     inbox_token: str
-#     timestamp: Optional[datetime.datetime]
-#     communication_id: int
-#     in_reply_to: Optional[int]
-#     bcc: Optional[int]
-#     cc: Optional[int]
-#     sender: int
-#     sendee: int
-#     subject_line: str
-#     content: str
-
-
-# class bcc(BaseModel):
-#     bcc_id: int
-#     inbox_token: str
-#     uuid: int
-
-
-# class cc(BaseModel):
-#     bcc_id: int
-#     inbox_token: str
-#     uuid: int
-
-
-# class inbox_conversation_start(BaseModel):
-#     sendee: int
-#     subject_line: str
-#     content: str
-#     bcc: Optional[list[int]]
-#     cc: Optional[list[int]]
 
 
 @router.post("/v1/inbox/start/{token}", tags=["inbox"])
@@ -56,6 +24,8 @@ async def start_a_new_conversation(
     token: str,
     inbox_conversation_start: inbox_conversation_start,
 ) -> json:
+    """this route allows someone to start a new conversation"""
+
     uuid = await get_token_user_id(token=token)
 
     inbox_token = await generate_token()
@@ -86,6 +56,7 @@ async def search_inbox(
     subject_line: str = None,
     content: str = None,
 ) -> json:
+    """this route returns the raw version of given content based upon the requested search parameters."""
     uuid = await get_token_user_id(token=token)
 
     sql = select(Inbox)
@@ -127,8 +98,110 @@ async def search_inbox(
     return HTTPException(status.HTTP_200_OK, detail=response)
 
 
+@router.get("/v1/inbox/id-search/", tags=["inbox"])
+async def search_inbox_id(
+    token: str,
+    inbox_id: int = None,
+    inbox_token: str = None,
+    sender: int = None,
+    sendee: int = None,
+    subject_line: str = None,
+    content: str = None,
+) -> json:
+    """this route returns the ids for the items found in the search, for light-weight searching"""
+
+    uuid = await get_token_user_id(token=token)
+
+    sql = select(Inbox)
+
+    if inbox_id:
+        sql = sql.where(Inbox.inbox_id == inbox_id)
+
+    if inbox_token:
+        sql = sql.where(Inbox.inbox_token == inbox_token)
+
+    if sender:
+        sql = sql.where(Inbox.sender == sender)
+
+    if sendee:
+        sql = sql.where(Inbox.sendee == sendee)
+
+    if subject_line:
+        sql = sql.where(Inbox.subject_line == subject_line)
+
+    if content:
+        sql = sql.where(Inbox.content == content)
+
+    async with USERDATA_ENGINE.get_session() as session:
+        session: AsyncSession = session
+        async with session.begin():
+            inbox_results = await session.execute(sql)
+
+    inbox_results = sqlalchemy_result(inbox_results)
+    inbox_results = inbox_results.rows2dict()
+
+    response = []
+    for result in inbox_results:
+        inbox_sender = result.get("sender")
+        inbox_sendee = result.get("sendee")
+        if (int(uuid) != inbox_sender) and (int(uuid) != inbox_sendee):
+            continue
+        response.append(result.get("inbox_id"))
+
+    return HTTPException(status.HTTP_200_OK, detail=response)
+
+
+@router.get("/v1/inbox/id-conversations/", tags=["inbox"])
+async def get_inbox_conversation_ids(token: str) -> json:
+    """
+    This route allows you to get the inbox_ids for all active conversations.
+    """
+    uuid = await get_token_user_id(token=token)
+    sql = select(Inbox)
+    sql = sql.where(or_(Inbox.sendee == uuid, Inbox.sender == uuid))
+
+    async with USERDATA_ENGINE.get_session() as session:
+        session: AsyncSession = session
+        async with session.begin():
+            inbox_results = await session.execute(sql)
+
+    inbox_results = sqlalchemy_result(inbox_results)
+    inbox_results = inbox_results.rows2dict()
+
+    response = list(set([result.get("inbox_token") for result in inbox_results]))
+
+    return HTTPException(status.HTTP_200_OK, detail=response)
+
+
+@router.get("/v1/inbox/overview/", tags=["inbox"])
+async def get_inbox_overview(token: str) -> json:
+    """
+    Returns an overview of the active conversations and the most recent message in the conversation list. This is ideal for overviews.
+    """
+    uuid = await get_token_user_id(token=token)
+    sql = select(Inbox)
+    sql = sql.where(or_(Inbox.sendee == uuid, Inbox.sender == uuid))
+
+    async with USERDATA_ENGINE.get_session() as session:
+        session: AsyncSession = session
+        async with session.begin():
+            inbox_results = await session.execute(sql)
+
+    inbox_results = sqlalchemy_result(inbox_results)
+    inbox_results = inbox_results.rows2dict()
+
+    inbox_results.sort(key=operator.itemgetter("inbox_token"))
+    groups = itertools.groupby(inbox_results, key=operator.itemgetter("inbox_token"))
+    response = dict((cls, list(items)[-1]) for cls, items in groups)
+
+    return HTTPException(status.HTTP_200_OK, detail=response)
+
+
 @router.post("/v1/inbox/reply/{token}", tags=["inbox"])
 async def reply_to_a_conversation(token: str, inbox_token: str, content: str) -> json:
+    """
+    This route allows you to send a reply to a given inbox token/conversation.
+    """
     uuid = await get_token_user_id(token=token)
 
     sql_inbox = insert(Inbox).values(
@@ -149,6 +222,9 @@ async def reply_to_a_conversation(token: str, inbox_token: str, content: str) ->
 
 @router.get("/v1/inbox/{token}", tags=["inbox"])
 async def get_inbox_information(token: str) -> json:
+    """
+    This route allows you to obtain all up to date inbox information for a user, with all conversations.
+    """
     uuid = await get_token_user_id(token=token)
 
     sql = select(Inbox)
@@ -163,4 +239,8 @@ async def get_inbox_information(token: str) -> json:
     inbox_results = sqlalchemy_result(inbox_results)
     inbox_results = inbox_results.rows2dict()
 
-    return HTTPException(status.HTTP_200_OK, detail=inbox_results)
+    inbox_results.sort(key=operator.itemgetter("inbox_token"))
+    groups = itertools.groupby(inbox_results, key=operator.itemgetter("inbox_token"))
+    response = dict((cls, list(items)) for cls, items in groups)
+
+    return HTTPException(status.HTTP_200_OK, detail=response)
